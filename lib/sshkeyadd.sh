8082527ac8b81ed8dee86a9180cdaab5a34ebca3
Ignacio Rodríguez <ignacio@sugarlabs.org>
2017-10-25 20:27:14 -0300

    grant permissions to pi user on .ssh folder

--- lib/sshkeyadd.sh
+++ lib/sshkeyadd.sh
@@ -5,6 +5,7 @@ chmod 700 /root/.ssh /home/pi/.ssh
 
 echo "$@" >> /home/pi/.ssh/authorized_keys
 chmod 600 /home/pi/.ssh/authorized_keys
+chown -R pi:pi /home/pi/.ssh
 
 echo "$@" >> /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
